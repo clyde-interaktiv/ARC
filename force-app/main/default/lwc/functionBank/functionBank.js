7978a17aac1341d982a8e250ce856976c13b12e5
Clyde Tan <clydeenriquetan@gmail.com>
2022-06-13 22:23:58 +0800

    updated components from jap

--- force-app/main/default/lwc/functionBank/functionBank.js
+++ force-app/main/default/lwc/functionBank/functionBank.js
@@ -1,7 +1,6 @@
 import generateRandomId from './generateRandomId'
 
 function getDateString(dateParam){
-    console.log(`🚀\n ~ file: functionBank.js ~ line 4 ~ dateParam`, dateParam)
     const newDate = new Date(dateParam)
     const year = newDate.getFullYear()
     let month = newDate.getMonth() + 1
@@ -9,10 +8,6 @@ function getDateString(dateParam){
 
     Number(month) < 10 ? month = '0'+month : null
     Number(date) < 10 ? date = '0'+date: null
-
-    console.log(`🚀\n ~ file: functionBank.js ~ line 15 ~ month`, month)
-    console.log(`🚀\n ~ file: functionBank.js ~ line 17 ~ date`, date)
-
 
     return `${year}-${month}-${date}`
 }
